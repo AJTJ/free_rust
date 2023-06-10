afa154a80281b4343cadc6f50de271aa2472ad38
AJTJ <janke.aaron@gmail.com>
2023-06-10 17:11:19 -0600

    update Sat 10 Jun 2023 17:11:19 CST

--- src/schema.rs
+++ src/schema.rs
@@ -52,4 +52,8 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(dive_sessions, dives, users,);
+diesel::allow_tables_to_appear_in_same_query!(
+    dive_sessions,
+    dives,
+    users,
+);
